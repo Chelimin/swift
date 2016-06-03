e6c6aa7d2531176fcbd65eac3c19cf17406d1589
He Limin <chelimin@gmail.com>
2016-06-04 01:16:39 +0800

    add quotes project

---
+++ InspireMe/InspireMe/Quote.swift
@@ -0,0 +1,22 @@
+//
+//  Quote.swift
+//  InspireMe
+//
+//  Created by He Limin on 3/6/16.
+//  Copyright © 2016 Cat. All rights reserved.
+//
+
+import Foundation
+
+class Quote{
+    
+    var quote: String
+    var author: String
+    
+    init(quote: String, author: String){
+        self.quote = quote
+        self.author = author
+        
+    }
+    
+}
