e28594f12f389aacbb7540b6f1750d5f97ac35b3
He Limin <chelimin@gmail.com>
2016-06-11 18:34:45 +0800

    add playground code

---
+++ SGFoodApp/SGFoodApp.playground/Contents.swift
@@ -0,0 +1,42 @@
+////: Playground - noun: a place where people can play
+//
+//import UIKit
+//
+//var str = "Hello, playground"
+//
+//print(str)
+//
+//let myNumber:Int = 10
+//
+//for character in "pizza".characters {
+//    print(character)
+//}
+//
+//for character in "😊☺️☺️".characters{
+//    print(character)
+//}
+//
+//var str1 = "😊"
+//var test = str1 + "😊"
+//print("hello \(test)")
+//
+//
+//var arrayNames = ["1", "2", "3", "4"]
+//print (arrayNames)
+//
+//print ("value of second element is \(arrayNames[1])")
+//
+//var myDict = ["name": "Cat", "company": "HouseHelp", "job":"Manager"]
+//print(myDict["name"]!)
+//print(Array(myDict.keys))
+//
+//var foodDict = [0: ["name": "Chicken Rice", "des": "Chicken with rice"],
+//                1: ["name": "Chilli Crab", "des": "Chilli with crab"],
+//                2: ["name": "Laksa", "des": "Noodles with curry"],
+//                3: ["name": "Nasi Lemak", "des": "Coconut rice"],]
+//
+//print(foodDict[0]!)
+
+
+
+
