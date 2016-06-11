7fd56903144251ceadbadea747c20cca040fb17b
He Limin <chelimin@gmail.com>
2016-06-11 18:32:17 +0800

    add SGFoodApp

---
+++ SGFoodApp/HelloWorld/DetailsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DetailsViewController.swift
+//  HelloWorld
+//
+//  Created by He Limin on 11/6/16.
+//  Copyright © 2016 Cat. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    var foodName: String?
+    var foodDes: String?
+    var foodImgName: String?
+    
+    @IBOutlet weak var foodNameLabel: UILabel!
+    
+    @IBOutlet weak var foodThumbImage: UIImageView!
+    
+    
+    @IBOutlet weak var foodDesTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        foodNameLabel.text = foodName!
+        foodDesTextView.text = foodDes!
+        foodThumbImage.image = UIImage(named: foodImgName!)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SGFoodApp/HelloWorld/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by He Limin on 11/6/16.
+//  Copyright © 2016 Cat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var name=""
+
+    @IBOutlet weak var helloDes: UILabel!
+    
+    @IBOutlet weak var helloText: UITextField!
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    @IBAction func helloButton(sender: AnyObject) {
+//        helloLabel.text = helloText.text
+//        helloLabel.text = String(sender.tag)
+        helloLabel.text = arrayFood[sender.tag]
+        helloDes.text = arrayDes[sender.tag]
+        
+        name = arrayFood[sender.tag]
+        
+            }
+    
+    
+    var arrayFood = ["Chicken rice", "Chilli crab", "Laksa", "Nasi Lemak"]
+    var arrayDes = ["Chicken with rice", "Chilli with crab", "Noodles with curry","Coconut rice"]
+    var arrayImg = ["chickenrice", "chillicrab","laksa","nasilemak"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "My App"
+        
+        print("this works")
+        helloLabel.text = "hello world"
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        guard let tagIndex = sender?.tag else{
+            return
+        }
+        
+        let destinationVC = segue.destinationViewController as? DetailsViewController
+        
+//        destinationVC?.foodName  = name
+        destinationVC?.foodName = arrayFood[tagIndex]
+        destinationVC?.foodDes = arrayDes[tagIndex]
+        destinationVC?.foodImgName = arrayImg[tagIndex]
+        
+    }
+
+
+}
+
